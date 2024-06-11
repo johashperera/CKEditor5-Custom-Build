cce9a07d1e36c3c5f0565c10598d38d4873b2b24
Johash Perera <johash.dev@zohomail.com>
2024-06-11 13:27:15 +0530

    remove unwanted type declarations

--- build/delete-mention-plugin.d.ts
+++
@@ -1,4 +0,0 @@
-import Plugin from "@ckeditor/ckeditor5-core/src/plugin";
-export declare class MentionDeletePlugin extends Plugin {
-    init(): void;
-}
--- build/mention-delete-plugin.d.ts
+++
@@ -1,5 +0,0 @@
-import Plugin from '@ckeditor/ckeditor5-core/src/plugin';
-declare class MentionDeletePlugin extends Plugin {
-    init(): void;
-}
-export default MentionDeletePlugin;
