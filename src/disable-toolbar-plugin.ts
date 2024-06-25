2d26b11d92953dce0dc51b6835a679d630048626
Johash Perera <johash.dev@zohomail.com>
2024-06-25 15:27:17 +0530

    add toolbar filtering to disable toolbar plugin

--- src/disable-toolbar-plugin.ts
+++ src/disable-toolbar-plugin.ts
@@ -1,17 +1,29 @@
-import { Plugin } from '@ckeditor/ckeditor5-core';
+import { Plugin } from "@ckeditor/ckeditor5-core";
 
 export class DisableToolbar extends Plugin {
   init() {
     const editor = this.editor;
-
-    editor.model.document.on('change', () => {
-      const selectedElement = editor.model.document.selection.getSelectedElement();
+    const toolsToDisable = [
+      "numberedList",
+      "menuBar:numberedList",
+      "bulletedList",
+      "menuBar:bulletedList",
+      "todoList",
+      "menuBar:todoList",
+    ];
+    editor.model.document.on("change", () => {
+      const selectedElement =
+        editor.model.document.selection.getSelectedElement();
       const toolbarItems = Array.from(editor.ui.componentFactory.names());
 
       toolbarItems.forEach((itemName) => {
-        const command = editor.commands.get(itemName as string);
-        if (command) {
-          command.isEnabled = !(selectedElement && selectedElement.name === 'imageBlock');
+        if (toolsToDisable.includes(itemName)) {
+          const command = editor.commands.get(itemName as string);
+          if (command) {
+            command.isEnabled = !(
+              selectedElement && selectedElement.name === "imageBlock"
+            );
+          }
         }
       });
     });
